7db67b901758464d2984416a5502f173efa41c9d
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-12 01:11:40 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,36 @@
 # Write your code here.
+
+ katz_deli = []
+ 
+ def line(array)
+  if array.length >= 1
+    new_array = []
+    counter = 1 
+    array.each do |name|
+      new_array.push("#{counter}. #{name}")
+      counter += 1
+    end  
+   puts "The line is currently: #{new_array.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end  
+end
+
+def take_a_number(array, name)
+  array.push(name)
+  puts "Welcome, #{name}. You are number #{array.length} in line."
+
+end
+
+def now_serving(array)
+  if array.length == 0 
+    
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{array.shift}."
+    
+  end
+end 
+
+
+
